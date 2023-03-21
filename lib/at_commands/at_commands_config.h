7d73b0fe30e3df0c084c37b448b9681dc0432f46
Dror Gluska <drorgl@yahoo.com>
2023-03-21 11:55:42 +0200

    add logging

---
+++ lib/at_commands/at_commands_config.h
@@ -0,0 +1,12 @@
+// CMUX/TS 0710 Client/Server Copyright (C) 2022 Dror Gluska
+#pragma once
+
+/// \cond
+#include <stdio.h>
+/// \endcond
+
+#define CMUX_LOG_TRACE(format, ...) 
+// #define CMUX_LOG_TRACE(format, ...) printf("%s:%d:%s [TRACE] ",__FILE__, __LINE__, __func__); printf(format"\n",##__VA_ARGS__);
+#define CMUX_LOG_DEBUG(format, ...) printf("%s:%d:%s [DEBUG] ",__FILE__, __LINE__, __func__); printf(format"\n",##__VA_ARGS__);
+#define CMUX_LOG_ERROR(format, ...) printf("%s:%d:%s [ERROR] ",__FILE__, __LINE__, __func__); printf(format"\n",##__VA_ARGS__);
+#define CMUX_LOG_WARN(format, ...) printf("%s:%d:%s [WARN ] ",__FILE__, __LINE__, __func__); printf(format"\n",##__VA_ARGS__);
